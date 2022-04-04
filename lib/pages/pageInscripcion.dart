6b9b2a4b2730cd4c308a6a12cdcb3b2555b62430
OrlandoRomero7 <al18760462@ite.edu.mx>
2022-04-04 16:37:01 -0700

    quientando error de inscripcion

--- lib/pages/pageInscripcion.dart
+++ lib/pages/pageInscripcion.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Inscripcion extends StatefulWidget {
@@ -11,42 +12,50 @@ class _InscripcionState extends State<Inscripcion> {
   @override
   Widget build(BuildContext context) {
     const primaryColor = Color(0xFF24264C);
-    
+
     return Scaffold(
-        //drawer: Drawers(),//desprende el drawer en la page
-        appBar: AppBar(
-          title: const Text("Proceso de admisión"),
-          centerTitle: true,
-      backgroundColor: const Color.fromARGB(255, 29, 58, 105,),
-    ),
-    body: ListView(
+      //drawer: Drawers(),//desprende el drawer en la page
+      appBar: AppBar(
+        title: const Text("Proceso de admisión"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(
+          255,
+          29,
+          58,
+          105,
+        ),
+      ),
+      /* body: ListView(
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        children: [
+          //Padding(padding: EdgeInsets.fromLTRB(70, 30, 40, 0)),
+           Text('Proceso de admisión \n',
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.bold,
+              )),
 
-          children: [
-            //Padding(padding: EdgeInsets.fromLTRB(70, 30, 40, 0)),
-            Text('Proceso de admisión \n',style:TextStyle(
-              fontSize: 35.0,fontWeight: FontWeight.bold,) ),
-
-            Text('Sistema Integral Escolar  (SIE) ingresar en el siguiente enlace👇👇\n',
-
-    style:TextStyle(
-              fontSize: 15.0,)),
-            CupertinoButton(child: Text('http://escolares.ensenada.tecnm.mx\n'), onPressed: () {
-              launch("http://escolares.ensenada.tecnm.mx");
-            }),
-            new Image.network(
-              'https://www.ensenada.tecnm.mx/wp-content/uploads/2022/03/Proceso-de-admision-2022-2.jpeg',
-            ),
+          Text(
+              'Sistema Integral Escolar  (SIE) ingresar en el siguiente enlace👇👇\n',
+              style: TextStyle(
+                fontSize: 15.0,
+              )),
+              /*
+          CupertinoButton(
+              child: const Text('http://escolares.ensenada.tecnm.mx\n'),
+              onPressed: () {
+                launch("http://escolares.ensenada.tecnm.mx");
+              }),
+          new Image.network(
+            'https://www.ensenada.tecnm.mx/wp-content/uploads/2022/03/Proceso-de-admision-2022-2.jpeg',
+          ),*/
 /*
             new Image.asset(
               'assets/images/convocatoria.png',
 
             ),*/
-
-
-          ],
-
-      ),
+        ],
+      ),*/
     );
   }
 }
